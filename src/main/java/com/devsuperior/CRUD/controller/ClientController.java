c46ec48153100c8bda990d429c2954818840e46b
Romulo P T Mota <romuloptmota@gmail.com>
2025-04-04 17:01:39 -0300

    Criado busca de clients

--- src/main/java/com/devsuperior/CRUD/controller/ClientController.java
+++ src/main/java/com/devsuperior/CRUD/controller/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.CRUD.controller;
 import com.devsuperior.CRUD.dto.ClientDTO;
 import com.devsuperior.CRUD.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,5 +19,11 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO clientDTO = service.findById(id);
         return ResponseEntity.ok(clientDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> clientDTOPage = service.findAll(pageable);
+        return ResponseEntity.ok(clientDTOPage);
     }
 }
--- src/main/java/com/devsuperior/CRUD/service/ClientService.java
+++ src/main/java/com/devsuperior/CRUD/service/ClientService.java
@@ -4,6 +4,8 @@ import com.devsuperior.CRUD.dto.ClientDTO;
 import com.devsuperior.CRUD.entities.Client;
 import com.devsuperior.CRUD.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> clients = repository.findAll(pageable);
+        return clients.map(client -> new ClientDTO(client));
     }
 }
