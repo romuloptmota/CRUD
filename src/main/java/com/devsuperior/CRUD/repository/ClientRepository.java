d171489dd78848dcd406d2f236dcd395f8aed283
Romulo P T Mota <romuloptmota@gmail.com>
2025-04-04 16:18:38 -0300

    Criado repository de client, entity de client e configurado banco h2

---
+++ src/main/java/com/devsuperior/CRUD/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.CRUD.repository;
+
+import com.devsuperior.CRUD.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
